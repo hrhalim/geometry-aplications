db7c1c346bf5f3a4504e7abd3e9840826f758078
hrhalim <hrhalimbd8624@gmail.com>
2023-02-18 01:50:44 +0600

    added all event

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Event for Triangle
 document.getElementById('btn-triangle').addEventListener('click', function(){
-   const inputBase =  getInputValueId('input-base');
-   const inputHeight =  getInputValueId('input-height');
+   const inputBase = getInputValueId('input-base');
+   const inputHeight = getInputValueId('input-height');
    const TotalTriangle = (0.5 * inputBase * inputHeight).toFixed(2);  
    getResutlList('triangle-title', TotalTriangle);
 
@@ -10,27 +10,46 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
 
 // Event for Rectangle
 document.getElementById('btn-rectangle').addEventListener('click', function(){
-    const inputwidth =  getInputValueId('input-width');
-   const inputLength =  getInputValueId('input-length');
+    const inputwidth = getInputValueId('input-width');
+   const inputLength = getInputValueId('input-length');
    const totalRectangle = (inputwidth * inputLength).toFixed(2);  
    getResutlList('rectangle-title', totalRectangle);
  
  });
 
- // Event for Parallelogram
-document.getElementById('btn-paralleogram').addEventListener('click', function(){
-    const parallelogramBase =  getInputValueId('parallelogram-base');
-    const parallelogramHeigh =  getInputValueId('parallelogram-heigh');
+//  Event for Parallelogram
+document.getElementById('btn-parallelogram').addEventListener('click', function(){
+    const parallelogramBase = getInputValueId('parallelogram-base');
+    const parallelogramHeigh = getInputValueId('parallelogram-height');
     const totalParallelogram = (parallelogramBase * parallelogramHeigh).toFixed(2);  
    getResutlList('parallelogram-title', totalParallelogram); 
 
  });
 
-  // Event for Rhombus
+// Event for Rhombus
 document.getElementById('btn-rhombus').addEventListener('click', function(){
-    const inputDiagonalOne =  getInputValueId('diagonal-one');
-    const inputDiagonalTwo =  getInputValueId('diagonal-two');
-    const totalDiagonal = (0.5 * parallelogramBase * parallelogramHeigh).toFixed(2);  
+    const inputDiagonalOne =  getInputValueId('input-diagonal-one');
+    const inputDiagonalTwo =  getInputValueId('input-diagonal-two');
+    const totalDiagonal = (0.5 * inputDiagonalOne * inputDiagonalTwo).toFixed(2);  
    getResutlList('rhombus-title', totalDiagonal); 
 
  });
+
+// Event for Pentagon
+document.getElementById('btn-pentagon').addEventListener('click', function(){
+    const inputPentagonOne =  getInputValueId('input-pentagon-one');
+    const inputPentagonTwo =  getInputValueId('input-pentagon-two');
+    const totalPentagon = (0.5 * inputPentagonOne * inputPentagonTwo).toFixed(2);  
+   getResutlList('pentagon-title', totalPentagon); 
+
+ });
+
+// Event for Ellipse
+document.getElementById('btn-ellipse').addEventListener('click', function(){
+    const inputEllipseOne =  getInputValueId('input-ellipse-one');
+    const inputEllipseTwo =  getInputValueId('input-ellipse-two');
+    const totalEllipse = (3.14159265 * inputEllipseOne * inputEllipseTwo).toFixed(2);  
+   getResutlList('ellipse-title', totalEllipse); 
+
+ });
+
